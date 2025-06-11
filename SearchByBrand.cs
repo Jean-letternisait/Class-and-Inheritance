2346b625222ca5e6ae4ddf7e202f8b7e40fad51a
Athul Jose <athuljose88@gmail.com>
2025-06-11 14:37:40 -0600

    Add files via upload

---
+++ SearchByBrand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+static void FindByBrand(List<Appliance> appliances)
+{
+    Console.Write("Enter brand to search for: ");
+    string brand = Console.ReadLine();
+
+    var matches = appliances.Where(a => a.Brand.Equals(brand, StringComparison.OrdinalIgnoreCase));
+
+    if (!matches.Any())
+    {
+        Console.WriteLine("No appliances found for that brand.");
+    }
+    else
+    {
+        Console.WriteLine("Matching Appliances:");
+        foreach (var appliance in matches)
+        {
+            Console.WriteLine(appliance);
+            Console.WriteLine("--------------------");
+        }
+    }
+}
